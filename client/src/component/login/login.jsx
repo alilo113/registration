fcb66a80375fa92a332efb2e14440cdc7e3bbc10
unknown <aliopdandan@gmail.com>
2023-12-01 19:09:48 +0100

    commit

--- client/src/component/login/login.jsx
+++ client/src/component/login/login.jsx
@@ -1,22 +1,67 @@
 import { Link } from "react-router-dom";
-import React from 'react';
+import React, { useState } from 'react';
 
 export function Login(){
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    async function handleSubmit(e){
+        e.preventDefault();
+    
+        try {
+          const response = await fetch('http://localhost:5000/login', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password }),
+          });
+    
+          if (response.ok) {
+            window.location.href = '/dashboard'; // Redirect upon successful login
+          } else {
+            const errorMessage = await response.text();
+            setError(errorMessage || 'Invalid username or password.');
+          }
+        } catch (error) {
+          console.error('Error occurred:', error);
+          setError('An error occurred. Please try again.');
+        }
+    }
+        
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-200">
             <div className="max-w-md w-full p-6 bg-white rounded-lg shadow-md">
                 <h1 className="text-2xl font-semibold mb-6 text-center">Log in</h1>
-                <form action="/signup" method="post">
+                <form onSubmit={handleSubmit}>
                     <div className="mb-4">
                         <label htmlFor="username" className="block text-gray-700 font-bold mb-2">Username</label>
-                        <input type="text" id="username" name="username" className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500" required />
+                        <input
+                            type="text"
+                            id="username"
+                            name="username"
+                            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            required
+                        />
                     </div>
                     <div className="mb-4">
-                        <label htmlFor="username" className="block text-gray-700 font-bold mb-2">Password</label>
-                        <input type="text" id="username" name="username" className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500" required />
+                        <label htmlFor="password" className="block text-gray-700 font-bold mb-2">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
                     </div>
+                    <div className="mb-4 text-red-500">{error}</div>
                     <div className="mb-6">
-                        <button type="submit" className="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Sign Up</button>
+                        <button type="submit" className="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Log In</button>
                     </div>
                 </form>
                 <Link to="/sign-up">Create an account</Link>
--- server/server.js
+++ server/server.js
@@ -2,44 +2,51 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const user = require('./models/userSchema');
-const cors = require('cors');
 
-// Middleware to parse incoming JSON data
-app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/users')
+mongoose.connect('mongodb://127.0.0.1:27017/usersDatabase')
   .then(() => {
     console.log('Connected to mongo database');
+    // Your code for database operations goes here
   })
   .catch((error) => {
     console.error('Error connecting to MongoDB:', error);
   });
 
+// Middleware to handle CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Set the allowed origin (* for all)
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.post('/api/users', async (req, res) => {
   try {
     const { username, email, password } = req.body;
-  
     // Perform data validation checks here if necessary
-  
-    // Create a new user in the database using the 'user' model
     const newUser = new user({ Username: username, Email: email, Password: password });
+    console.log(newUser)
     const savedUser = await newUser.save();
-  
-    res.status(201).json({ message: 'User created successfully', user: savedUser });
   } catch (error) {
     console.error('Error creating user:', error);
-    res.status(500).json({ error: 'Error creating user', message: error.message });
   }
 });  
 
-app.get('/api/users', async (req, res) => {
+app.post('/login', async (req, res) => {
   try {
-    const users = await user.find();
-    res.json({ users });
+    const { username, password } = req.body;
+    const existingUser = await user.findOne({ Username: username, Password: password });
+
+    if (existingUser) {
+      res.status(200).send('Login successful!');
+    } else {
+      res.status(401).send('Invalid username or password.');
+    }
   } catch (error) {
-    console.error('Error fetching users:', error);
-    res.status(500).json({ error: 'Error fetching users', message: error.message });
+    console.error('Error during login:', error);
+    res.status(500).send('An error occurred during login.');
   }
 });
 
